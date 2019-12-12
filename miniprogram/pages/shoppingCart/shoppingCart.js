c02cf03454a97bb4a52570d902e4ee5803a74275
xiexie2015 <626993952@qq.com>
2019-12-12 15:27:08 +0800

    优化分类功能

--- miniprogram/pages/shoppingCart/shoppingCart.js
+++ miniprogram/pages/shoppingCart/shoppingCart.js
@@ -1,19 +1,12 @@
 // miniprogram/pages/shoppingCart/shoppingCart.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     orders: [],
     selected: true,
     selectedAll: true,
     totalPrice: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function(options) {
     this.loadOrders();
   },
